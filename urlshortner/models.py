d94424ebab187a445daf3c1c71a5a610d277838a
Nitesh Verma <niteshghn@gmail.com>
2018-08-04 23:13:56 +0530

    fixed broken link

--- urlshortner/models.py
+++ urlshortner/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class UrlKeyHash(models.Model):
     key = models.CharField(max_length=16, db_index=True, primary_key=True)
     url = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.PROTECT,null=True)
     created_on = models.DateTimeField(auto_now_add=True)
     is_private = models.BooleanField(default=False)
     hits = models.IntegerField(default=0)
